4a5728ced731d8bbb0966e0cea3dfc0b7dada6b7
Greg Borota <borota@gmail.com>
2017-11-01 21:21:56 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.jpg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Peshitta</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to Peshitta Web Application.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
